7b33d6ab2bc4b49bcc3d4348528646d9dada86ed
David Estlund <destlund@gmail.com>
2018-01-31 19:21:11 -0600

    got it serving static files, need to fix html files

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var fs = require("fs");
 
 var app = express();
 var PORT = 3000;
 
-
+app.use(express.static(__dirname))
 
 
 
