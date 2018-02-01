06c5ce0d443ee928f8bcac9e4da74105541f7b1f
Pete <petegarcia54@sbcglobal.net>
2018-01-31 19:35:23 -0600

    Working directories

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
+//Dependencies//
+
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+//Dependencies//
 
+//Opening Express//
 var app = express();
 var PORT = 3000;
+//Opening Express//
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
 
 
 
