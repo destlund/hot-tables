9fea995d9a64c3f149e960fe84adb39f2ce51455
David Estlund <destlund@gmail.com>
2018-01-31 19:40:43 -0600

    pseudocoded our current tasks

--- server.js
+++ server.js
@@ -15,3 +15,15 @@ app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 
 });
+
+// take the input from the reserve form and use the table constructor to create a table object
+
+
+// depending on how many tables are already reserved, add it to the reservation list or the waitlist
+
+
+// replace stuff in the table list on the tables page to display regular tables
+
+
+// replace stuff in the waitlist on the tables page to display the waitlist
+
